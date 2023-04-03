193a7b58ac28b1e2851f8a29912cf37fedac71cc
rhishikr <rhishik.royninabh@ucalgary.ca>
2023-04-03 02:03:16 -0600

    updated CleaningTask

--- FinalProject/edu/ucalgary/oop/CleaningTask.java
+++ FinalProject/edu/ucalgary/oop/CleaningTask.java
@@ -1,3 +1,17 @@
 public class CleaningTask {
-    
+    private int cleanTime;
+
+    public CleaningTask(String name) {
+        if (name.equalsIgnoreCase("c") || name.equalsIgnoreCase("f") || name.equalsIgnoreCase("r")) {
+            cleanTime = 5;
+        } else if (name.equalsIgnoreCase("p")) {
+            cleanTime = 10;
+        } else {
+            System.out.println("Invalid animal name for cleaning task.");
+        }
+    }
+
+    public int getCleanTime() {
+        return cleanTime;
+    }
 }
--- FinalProject/edu/ucalgary/oop/FeedingTask.java
+++ FinalProject/edu/ucalgary/oop/FeedingTask.java
@@ -1,3 +1,16 @@
 public class FeedingTask {
-    
+    private int numberAnimal;
+    private int feedTime;
+
+    public void FeedingTask(String name){
+
+    }
+
+    public int getFeedTime() {
+        return feedTime;
+    }
+
+    public int getNumberAnimal() {
+        return numberAnimal;
+    }
 }
