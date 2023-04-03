6497f6b85e0334c567e08ec78e3b3b5516233580
MusaJawad <musa.jawad@ucalgary.ca>
2023-04-03 00:25:19 -0600

    DataBase functional. Added test file for testing

---
+++ FinalProject/edu/ucalgary/oop/Test.java
@@ -0,0 +1,16 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.*;
+
+
+
+public class Test {
+
+    public Test() {
+    }
+
+    //put all the tests here
+
+   
+
+}
