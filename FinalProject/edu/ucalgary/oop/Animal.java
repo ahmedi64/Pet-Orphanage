9179f8629d4958c144703de57415ff306356e725
Abrar018 <abrar.rehan@ucalgary.ca>
2023-04-03 17:06:00 -0600

    Added Animal.java

--- FinalProject/edu/ucalgary/oop/Animal.java
+++ FinalProject/edu/ucalgary/oop/Animal.java
@@ -1,3 +1,35 @@
-public class Animal {
-    
+package FinalProject.edu.ucalgary.oop;
+public class Animal{
+    public String name;
+    public int numanimal;
+    public int tobefed;
+    public int feedtime;
+    public int cleantime;
+    private FeedingTask feed;
+    private CleaningTask clean;
+    public animal(String name){
+        this.name=name;
+        this.feed = new FeedingTask(name);
+        this.clean = new CleaningTask(name);
+        this.numanimal =feed.getnumberanimals();
+        this.tobefed = feed.getnumberanimals();
+        this.feedtime = feed.getfeedtime();
+        this.cleantime = feed.getcleantime();
+
+    }
+    public void decTobefed() {
+        this.tobefed -= this.tobefed;
+    }
+
+    public int getTobefed() {
+        return tobefed;
+    }
+
+    public int getCleantime() {
+        return cleantime;
+    }
+
+    public int getFeedtime() {
+        return feedtime*tobefed;
+    }
 }
