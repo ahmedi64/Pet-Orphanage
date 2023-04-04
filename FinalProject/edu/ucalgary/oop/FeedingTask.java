25a4fa28054b61b1a0c55a7ababfe0367d67ad93
rhishikr <rhishik.royninabh@ucalgary.ca>
2023-04-04 01:20:51 -0600

    updating feedingTask class

--- FinalProject/edu/ucalgary/oop/FeedingTask.java
+++ FinalProject/edu/ucalgary/oop/FeedingTask.java
@@ -1,9 +1,50 @@
+import java.sql.*;
+
 public class FeedingTask {
     private int numberAnimal;
     private int feedTime;
 
-    public void FeedingTask(String name){
+    public FeedingTask(String name) {
+        String species = name.toLowerCase();
+        try {
+            // Connect to the database
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/ewr", "root", "Jawad195");
 
+            // Query the database for the number of animals of the specified species
+            PreparedStatement stmt = conn.prepareStatement("SELECT COUNT(*) FROM ANIMALS WHERE AnimalSpecies=?");
+            stmt.setString(1, species);
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()) {
+                numberAnimal = rs.getInt(1);
+            } else {
+                System.out.println("No animals of species " + species + " found.");
+            }
+
+            // Set the feed time based on the species
+            switch(species) {
+                case "coyote":
+                    feedTime = 10 + 5 * numberAnimal;
+                    break;
+                case "fox":
+                    feedTime = 5 + 5*numberAnimal;
+                    break;
+                case "porcupine":
+                    feedTime = 5 * numberAnimal;
+                    break;
+                case "raccoon", "beaver":
+                    feedTime = 5;
+                    break;
+                default:
+                    System.out.println("Invalid animal name.");
+            }
+
+            // Close the database connection
+            rs.close();
+            stmt.close();
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public int getFeedTime() {
