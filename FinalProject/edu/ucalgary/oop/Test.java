02348caafa641af9f97c9cf6b9031cf92cfd187b
Abrar018 <abrar.rehan@ucalgary.ca>
2023-04-06 15:44:24 -0600

    Added Test.java and modified Animal.java

--- FinalProject/edu/ucalgary/oop/Test.java
+++ FinalProject/edu/ucalgary/oop/Test.java
@@ -1,13 +1,27 @@
 package FinalProject.edu.ucalgary.oop;
 
+import static org.junit.Assert.*;
 
 public class Test {
 
-    public Test() {
+    @org.junit.Test
+    public void testGetTobefed() {
+        Animal animal = new Animal("coyote");
+        assertEquals(animal.getTobefed(), animal.getNumAnimal());
     }
 
-    //put all the tests here
+    @org.junit.Test
+    public void testAnimalCBF() {
+        Animal animal = new Animal("porcupine");
+        int[] result = animal.animalCBF(25);
+        int[] expected = new int[]{4, 5};
+        assertArrayEquals(expected, result);
+    }
 
-   
-
+    @org.junit.Test
+    public void testDecTobefed() {
+        Animal animal = new Animal("raccoon");
+        animal.decTobefed();
+        assertEquals(animal.getTobefed(), animal.getNumAnimal() - 1);
+    }
 }
