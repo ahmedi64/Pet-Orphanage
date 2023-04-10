7642bb980c6a137c8fba6c3bab42ba82bcd673a6
ahmedi64 <ahmed.iqbal1@ucalgary.ca>
2023-04-09 18:54:04 -0600

    interface fix

--- FinalProject/edu/ucalgary/oop/FormatSchedule.java
+++ FinalProject/edu/ucalgary/oop/FormatSchedule.java
@@ -1,6 +1,6 @@
-// package FinalProject.edu.ucalgary.oop;
+package FinalProject.edu.ucalgary.oop;
 
 public interface FormatSchedule{
-    // Use this interface in order to format the final schedule so it looks proper
+    // Use this interface in order to format the final schedule so it is properly formatted
     public String getFormattedSchedule();
 }
