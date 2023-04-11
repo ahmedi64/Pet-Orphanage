6af2098a66344a1aa2319346798a38335cb4fee5
ahmedi64 <ahmed.iqbal1@ucalgary.ca>
2023-04-11 02:17:49 -0600

    added some documentation

--- FinalProject/edu/ucalgary/oop/FeedingTask.java
+++ FinalProject/edu/ucalgary/oop/FeedingTask.java
@@ -1,4 +1,6 @@
 // package FinalProject.edu.ucalgary.oop;
+//Members: Ahmed Iqbal, Musa Jawad, Abrar Rehan, Rishik Roy
+//Code version: 11.0.17
 
 import javax.naming.InvalidNameException;
 import java.sql.*;
--- FinalProject/edu/ucalgary/oop/ScheduleBuilder.java
+++ FinalProject/edu/ucalgary/oop/ScheduleBuilder.java
@@ -1,4 +1,6 @@
 // package FinalProject.edu.ucalgary.oop;
+//Members: Ahmed Iqbal, Musa Jawad, Abrar Rehan, Rishik Roy
+//Code version: 11.0.17
 
 import java.sql.*;
 import java.util.ArrayList;
